701f291ee272196ae1397a98778c43c63cce6c89
salah yousef <simyousef@gmail.com>
2018-08-12 21:35:18 +0200

    ListView component

---
+++ src/sidebar.js
@@ -0,0 +1,13 @@
+import React, {Component } from 'react'
+import './App.css';
+
+class Sidebar extends Component {
+    render () {
+        return (
+                <aside className="App-sidebar" >
+                </aside>
+        )
+    }
+}
+
+export default Sidebar
