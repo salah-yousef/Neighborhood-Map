9fce0c17f224c5c8aecc7f0abb9c7151e9d2b155
salah yousef <simyousef@gmail.com>
2018-08-12 21:35:08 +0200

    header component

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React, {Component } from 'react'
+import logo from './logo2.svg';
+import './App.css';
+
+class Header extends Component {
+    render () {
+        return (
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+        )
+    }
+}
+
+export default Header
