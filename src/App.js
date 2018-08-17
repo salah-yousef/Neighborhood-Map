ad29a41cf8d3ede970f6872f170a635f8929a288
salah yousef <simyousef@gmail.com>
2018-08-17 18:59:07 +0200

    the map and the app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import * as PlacesAPI from './PlacesAPI'
 import Header from './header'
-import Sidebar from './sidebar'
-import MapContainer from './MapContainer'
+import Sidebar from './Sidebar'
+import Map from './Map'
 import './App.css';
 
 class App extends Component {
-  state = {
-    places: []
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: []
+    }
+    this.itemClicked = this.itemClicked.bind(this);
+    this.toggleBounce = this.toggleBounce.bind(this);
+    this.filterMarkers = this.filterMarkers.bind(this);
 
+  }
 
   componentDidMount(){
     PlacesAPI.getAllPlaces(30.0444,31.2357,'cafe').then((places) => {
@@ -19,18 +25,71 @@ class App extends Component {
       console.log('Error While Getting All Locations')
       
     })
+  
+  }
+
+  toggleBounce(marker) {
+    console.log('bouncing?')
+    let google = this.props.google
+    if (marker.getAnimation() !== null && marker.getAnimation() !== undefined) {
+      marker.setAnimation(null)
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE)
+      setTimeout(function() {
+        marker.setAnimation(null)
+      }, 2100)
+    }
+  }
+
+  filterMarkers(filter) {
+    let places = [...this.state.places];
+    places.forEach((item) => {
+      if (item.name.toLowerCase().indexOf(filter.toLowerCase()) === -1) {
+        item.isVisible = false;
+        return;
+      }
+      item.isVisible = true;
+    });
+    this.setState({ 
+      places
+    });
+  }
+
+
+
+  itemClicked(itemID) {
+    let places = [...this.state.places];
+    places.forEach((item) => {
+      if (item.id === itemID) {
+        if (item.selected === undefined) {
+          item.selected = true;
+        } else {
+          item.selected = !item.selected;
+        }
+        return;
+      }
+      this.setState({ 
+        places,
+      });
+    });
   }
 
   render() {
     return (
       <div className="App flex-container">
         <div className="LeftSection">
-            <Sidebar />
+            <Sidebar 
+                  items = {this.state.places}
+                  itemClicked = {this.itemClicked}
+                  filterMarkers = {this.filterMarkers}  
+            />
         </div>
         <div className="RightSection">
             <Header />
             {
-              (this.state.places &&  <MapContainer 
+              (this.state.places &&  <Map
+                role="application"
+                toggleBounce={this.toggleBounce} 
                 places = {this.state.places} 
                 />)
               
